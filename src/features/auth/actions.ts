16affc8d37d02585cecb675db8214961973ed276
Jeronimo Galvez Mandujano <galvezjeronimo@gmail.com>
2026-03-20 02:38:43 -0500

    Cree la funcion de editar usario (pantalla, vfalidaciones con zod, y logica de negocio)

--- src/features/auth/actions.ts
+++ src/features/auth/actions.ts
@@ -1,8 +1,21 @@
 "use server";
 
 import { registerUser } from "@/server/services/userService";
-import { RegisterInput } from "@/types/auth.types";
+import { RegisterInput, UpdateUserInput } from "@/types/auth.types";
+import { auth } from "@/server/auth";
+import { updateUser } from "@/server/services/userService";
+import { redirect } from "next/navigation";
 
 export async function registerUserAction(data: RegisterInput) {
     return registerUser(data);
+}
+
+export async function updateUserAction(data: UpdateUserInput) {
+  const session = await auth()
+  if (!session?.user?.id) redirect("/login")
+
+  const result = await updateUser(Number(session.user.id), data)
+  if (result?.error) return { error: result.error }
+
+  redirect("/profile")
 }
